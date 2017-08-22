d1de25b3d965e9e08cc56687ee093e8107f6f30c
Kethen <kwchuiaa@connect.ust.hk>
2017-08-23 05:01:29 +0800

    allowing in memory bplist by supporting inputstream as input

--- src/test/java/nl/pvanassen/bplist/BinaryPListParserTest.java
+++ src/test/java/nl/pvanassen/bplist/BinaryPListParserTest.java
@@ -1,6 +1,9 @@
 package nl.pvanassen.bplist;
 
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 import nl.pvanassen.bplist.converter.ConvertToXml;
@@ -20,12 +23,28 @@ public class BinaryPListParserTest {
         assertNotNull(xmlElement);
         assertEquals(FileHelper.getContent(baseName + ".result"), xmlElement.getChildren().get(0).toString());
     }
+
+    private void testInputStreamMode(String baseName) throws IOException {
+        //List<BPListElement<?>> elements = elementParser.parseObjectTable(new FileInputStream(FileHelper.getFile(baseName + ".bplist")));
+        byte[] copyBuffer = new Byte[1024];
+        int copySize = 0;
+        FileInputStream testFile = new FileInputStream(FileHelper.getFile(baseName + ".bplist"));
+        ByteArrayOutputStream memory = new ByteArrayOutputStream();
+        while((copySize = testFile.read(copyBuffer, 0, 1024)) != 0){
+            memory.write(copyBuffer, 0, copySize);
+        }
+        testFile.close();
+        ByteArrayInputStream memoryStream = new ByteArrayInputStream(memory.toByteArray());
+        XMLElement xmlElement = convetToXml.convertToXml(memoryStream);
+        assertNotNull(xmlElement);
+        assertEquals(FileHelper.getContent(baseName + ".result"), xmlElement.getChildren().get(0).toString());
+    }
     
     @Test
     public void testAirplay() throws IOException {
         test("airplay");
     }
-    
+
     @Test
     public void testITunesSmall() throws IOException {
         test("iTunes-small");
@@ -48,5 +67,32 @@ public class BinaryPListParserTest {
     public void testUTF16() throws IOException {
         test("utf16");
     }
+    
+    @Test
+    public void testInputStreamModeAirPlay() throws IOException {
+        testInputStream("airplay");
+    }
+    
+    @Test
+    public void testInputStreamModeITunesSmall() throws IOException {
+        testInputStreamMode("iTunes-small");
+    }
 
+    @Test
+    public void testInputStreamModeSample1() throws IOException {
+        testInputStreamMode("sample1");
+    }
+    
+    @Test
+    public void testInputStreamModeSample2() throws IOException {
+        testInputStreamMode("sample2");
+    }
+    @Test
+    public void testInputStreamModeUID() throws IOException {
+        testInputStreamMode("uid");
+    }
+    @Test
+    public void testInputStreamModeUTF16() throws IOException {
+        testInputStreamMode("utf16");
+    }
 }
