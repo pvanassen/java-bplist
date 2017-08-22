7e2d909bc454dd8e5066bf42189423f5cd6944a9
Kethen <kwchuiaa@connect.ust.hk>
2017-08-23 03:58:33 +0800

    adding a new test case

--- src/test/java/nl/pvanassen/bplist/BinaryPListParserTest.java
+++ src/test/java/nl/pvanassen/bplist/BinaryPListParserTest.java
@@ -31,7 +31,7 @@ public class BinaryPListParserTest {
         assertNotNull(xmlElement);
         ByteArrayOutputStream outputBufferBOS = new ByteArrayOutputStream();
         PrintWriter outputBufferPW = new PrintWriter(outputBufferBOS);
-        convetToXml.dig(xmlElement);
+        convetToXml.dig(outputBufferPw, xmlElement, 0);
         outputBufferPW.close();
         String output = "";
         Scanner outputBufferScanner = new Scanner(new ByteArrayInputStream(outputBufferBOS.toByteArray()));
